bb9dd59d820d06bc494393809c57f3183778b46c
blbarr03 <bbarr0101@gmail.com>
2023-10-08 20:20:42 -0700

    adds graceful shutdown

--- application/app.go
+++ application/app.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
+
 	"github.com/redis/go-redis/v9"
 )
 
@@ -34,11 +36,32 @@ func (a* App) Start(ctx context.Context) error {
 		return fmt.Errorf("failed to connect to redis %w", err)	
 	}
 
+	defer func(){
+		if err := a.rdb.Close(); err != nil{
+			fmt.Println("failed to close redis")
+		}
+	}()
+
+
 	fmt.Printf("server listening on port %s", server.Addr)
 
+	ch := make(chan error, 1)
+	go func() {
 	err = server.ListenAndServe()
 	if err != nil{
-		return fmt.Errorf("failed to start server: %w", err) 
+		ch <- fmt.Errorf("failed to start server: %w", err) 
+	}
+	close(ch)
+	}()
+
+
+	select{
+		case err = <-ch:
+			return err
+		case <-ctx.Done():
+			timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+			defer cancel()
+			return server.Shutdown(timeout)
 	}
 	return nil
 }
